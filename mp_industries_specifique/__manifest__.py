b854412490db2f3dcf2754dd72171fc9b44beb26
Daniel Deyris <46327147+danieldeyris@users.noreply.github.com>
2022-02-15 20:02:42 +0100

    [REQ-0000000340] Ajouter les pièces attachées des articles sur les mails

---
+++ mp_industries_specifique/__manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Phidias : specifique Mp Industries",
+
+    'summary': """
+        Phidias : specifique Mp Industries
+        """,
+
+    'description': """
+        Phidias : specifique Mp Industries
+    """,
+
+    'author': "Phidias",
+    'website': "http://www.phidias.fr",
+    'category': 'Uncategorized',
+    'version': '15.0.0.2',
+
+    # any module necessary for this one to work correctly
+    'depends': [
+        'crm',
+        'sale',
+        'sale_management',
+        'account',
+        'stock',
+        'purchase',
+        'account_accountant',
+        'mrp',
+        'l10n_fr',
+        'l10n_fr_reports',
+        'l10n_fr_fec',
+        'web_studio',
+        'phi_order_mail_with_product_attachments',
+    ],
+    "data": [
+    ]
+}
